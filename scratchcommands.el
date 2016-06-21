11f8d7a9fdf65dd4e97475f30462ae97d147d90b
wagnermarques <wagnerdocri@gmail.com>
2016-06-21 03:19:02 -0300

    cleaning code because previows wrongs decisions

---
+++ scratchcommands.el
@@ -0,0 +1,13 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+(menu-bar-mode -1) ;hide emacs menu
+(menu-bar-mode 1) ;show emacs menu
+(tool-bar-mode -1) ;hide tool bar
+(tool-bar-mode 1) ;show tool bar
+
+
+
+
+
