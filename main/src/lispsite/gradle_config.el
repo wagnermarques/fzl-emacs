e2ddfb3da36faa8329dd36f78abe4221da8cae06
wagnermarques <wagnerdocri@gmail.com>
2020-06-19 09:14:03 -0400

    config_newsticker.el and gradle_config.el added

---
+++ main/src/lispsite/gradle_config.el
@@ -0,0 +1,17 @@
+;;; package --- Summary
+
+;;; Commentary:
+;;; https://github.com/jacobono/emacs-gradle-mode
+
+;;; Code:
+
+(require 'cl-lib)
+;; -*- coding: utf-8; lexical-binding: t; -*-
+
+
+(require 'gradle-mode)
+(gradle-mode 1)
+
+(provide 'gradle_config)
+;;; this_gradle_config.el ends here
+
