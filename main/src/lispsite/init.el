3b53971ee965d90b160a4a56d74fa47985174c1e
wagnermarques <wagnerdocri@gmail.com>
2023-02-04 23:46:52 -0300

    cleaning main just for README and index.org files

--- main/src/lispsite/init.el
+++
@@ -1,8 +0,0 @@
-(setq debug-on-error t)
-
-;; add this init.el dir to load-path
-;; it is needed to require another el files
-(normal-top-level-add-to-load-path '("."))
-(normal-top-level-add-subdirs-to-load-path)
-
-(require 'defaults-custom)
