50d1aa5f626c27ddd156da709cc503ef967a1470
wagnermarques <wagnerdocri@gmail.com>
2025-01-10 07:44:43 -0300

    rebasing to push Readme.org and coding-lang-php.el that makes dap-debug to works

--- main/src/lispsite/coding-lang-php.el
+++ main/src/lispsite/coding-lang-php.el
@@ -1,16 +1,9 @@
-;; Certifique-se de ter o use-package configurado
-;; Certifique-se de ter o use-package configurado
-(require 'use-package)
-(setq use-package-always-ensure t)
+(setq phpDebugJs "/media/wgn/d4ae1cfc-8228-4bec-a0cc-c6b7345e29bd/PROGSATIVOS/vscode-plugins/vscode-php-debug/out/phpDebug.js")
 
-;; Configuração do Company Mode
-(use-package company
-  :ensure t
-  :hook (after-init . global-company-mode)
-  :config
-  (setq company-tooltip-align-annotations t
-        company-minimum-prefix-length 1
-        company-idle-delay 0.0))  ; Ajuste o tempo de atraso conforme necessário
+;; Install use-package if not already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;; Configuração do lsp-mode com PHP
 (use-package lsp-mode
@@ -21,6 +14,26 @@
   (setq lsp-prefer-flymake nil)  ; Use Flycheck em vez de Flymake
   ;; Utilize company-capf como backend do company-mode para lsp-mode
   (add-to-list 'company-backends 'company-capf))
+
+(use-package dap-mode
+  :ensure t
+  :after (lsp-mode dap-mode)
+  :config 
+  (dap-auto-configure-mode)
+  ;; PHP debug template
+  (require 'dap-php)
+
+(dap-register-debug-template "PHP Xdebug"
+  (list :type "php"
+        :request "launch"
+        :name "Xdebug::Run"
+        :cwd "/media/wgn/EnvsBk/__devenv__/__devenv__eteczl/catracas-secretaria-yii2"
+        :program nil
+        :port 9003
+        :log t
+        :dap-server-log t
+        :dap-server-path '("node" "/media/wgn/d4ae1cfc-8228-4bec-a0cc-c6b7345e29bd/PROGSATIVOS/vscode-plugins/vscode-php-debug/out/phpDebug.js")))
+  
 
 (use-package lsp-ui
   :ensure t
@@ -33,18 +46,32 @@
   :hook (php-mode . lsp))
 
 ;; Install and configure dap-mode
-(use-package dap-mode
-  :ensure t
-  :after lsp-mode
-  :config (dap-auto-configure-mode)
-  ;; PHP debug template
-  (require 'dap-php)
-  (dap-register-debug-template "PHP Xdebug"
-                               (list :type "php"
-                                     :request "launch"
-                                     :name "Xdebug::Run"
-                                     :program nil
-                                     :cwd nil
-                                     :port 9003)))
+
+  ;; Enable logging
+  (setq dap-print-io t)  ; Enable printing of debug I/O to the *Messages* buffer
+  (setq dap-output-buffer-filter nil)  ; Disable output buffer filtering
+  (setq dap-output-buffer-compilation-mode t)  ; Enable compilation mode for output buffer
+
+  ;; To show logs in a separate buffer, you can use dap-output-buffer
+  (defun my/dap-mode-setup ()
+    (dap-ui-mode 1)
+    (add-hook 'dap-stopped-hook
+              (lambda (arg) (call-interactively #'dap-hydra)))
+    ;; Additional logging for debug session lifecycle
+    (add-hook 'dap-session-created-hook
+              (lambda (session) (message "Dap session created: %s" session)))
+    (add-hook 'dap-session-terminated-hook
+              (lambda (session) (message "Dap session terminated: %s" session)))
+    (add-hook 'dap-terminated-hook
+              (lambda (_session) (message "Dap terminated"))))
+
+  (add-hook 'dap-mode-hook 'my/dap-mode-setup))
+
+;; Custom function to refresh breakpoints with error handling
+(defun refresh-breakpoints-safe (&rest args)
+  (ignore-errors
+    (apply 'dap--refresh-breakpoints args)))
+
+(advice-add 'dap--refresh-breakpoints :around #'refresh-breakpoints-safe)
 
 (provide 'coding-lang-php)
