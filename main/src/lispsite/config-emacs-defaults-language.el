29678b0931422b6222642f338698c222c44e7ae0
wagnermarques <wagnerdocri@gmail.com>
2025-01-11 12:03:40 -0300

    corrige charset de acentuação ao salvar. Browser mostra corretamente arquivos acentuados

--- main/src/lispsite/config-emacs-defaults-language.el
+++ main/src/lispsite/config-emacs-defaults-language.el
@@ -5,14 +5,41 @@
 ;;This influences how Emacs handles language-specific features.
 ;;https://www.gnu.org/software/emacs/manual/html_node/emacs/International.html
 ;;https://www.gnu.org/software/emacs/manual/html_node/emacs/Language-Environments.html
-(setenv "LANG" "pt_BR.iso-8859-1")
-(set-language-environment "Latin-1")
+;;(setenv "LANG" "pt_BR.iso-8859-1")
+;;(set-language-environment "Latin-1")
 
 ;; Set language environment to Brazilian Portuguese
-(set-language-environment "Brazilian Portuguese")
+;;it must be consistent with os local
+;;wgn@wgnhost:/media/wgn/EnvsBk/__devenv__/fzl-emacs/main/src/lispsite$ locale
+;;LANG=pt_BR.UTF-8
+;;LANGUAGE=pt_BR:pt:en
+;;LC_CTYPE="pt_BR.UTF-8"
+;;LC_NUMERIC="pt_BR.UTF-8"
+;;LC_TIME="pt_BR.UTF-8"
+;;LC_COLLATE="pt_BR.UTF-8"
+;;LC_MONETARY="pt_BR.UTF-8"
+;;LC_MESSAGES="pt_BR.UTF-8"
+;;LC_PAPER="pt_BR.UTF-8"
+;;LC_NAME="pt_BR.UTF-8"
+;;LC_ADDRESS="pt_BR.UTF-8"
+;;LC_TELEPHONE="pt_BR.UTF-8"
+;;LC_MEASUREMENT="pt_BR.UTF-8"
+;;LC_IDENTIFICATION="pt_BR.UTF-8"
+;;LC_ALL=
+;(set-language-environment "Brazilian Portuguese")
 
-(prefer-coding-system 'iso-8859-1)
-(setq org-default-notes-encoding 'utf-8)
+;; Define a codificação padrão para novos buffers como UTF-8
+(set-language-environment "UTF-8")
+(setq default-buffer-file-coding-system 'utf-8)
+
+;; Força a codificação UTF-8 ao salvar arquivos
+(prefer-coding-system 'utf-8)
+(setq coding-system-for-write 'utf-8)
+
+
+;;(prefer-coding-system 'iso-8859-1)
+(prefer-coding-system 'utf-8)
+;;(setq org-default-notes-encoding 'utf-8)
 ;;(set-language-environment "pt_BR")
 ;;(insert (getenv "LANG"))pt_BR.UTF-8
 
@@ -43,27 +70,6 @@
 ;; Set font to one that supports Brazilian Portuguese characters
 ;;(set-frame-font "DejaVu Sans Mono-12" nil t)
 
-
-
-
-;;it must be consistent with os local
-;;wgn@wgnhost:/media/wgn/EnvsBk/__devenv__/fzl-emacs/main/src/lispsite$ locale
-;;LANG=pt_BR.UTF-8
-;;LANGUAGE=pt_BR:pt:en
-;;LC_CTYPE="pt_BR.UTF-8"
-;;LC_NUMERIC="pt_BR.UTF-8"
-;;LC_TIME="pt_BR.UTF-8"
-;;LC_COLLATE="pt_BR.UTF-8"
-;;LC_MONETARY="pt_BR.UTF-8"
-;;LC_MESSAGES="pt_BR.UTF-8"
-;;LC_PAPER="pt_BR.UTF-8"
-;;LC_NAME="pt_BR.UTF-8"
-;;LC_ADDRESS="pt_BR.UTF-8"
-;;LC_TELEPHONE="pt_BR.UTF-8"
-;;LC_MEASUREMENT="pt_BR.UTF-8"
-;;LC_IDENTIFICATION="pt_BR.UTF-8"
-;;LC_ALL=
-(set-language-environment "Brazilian Portuguese")
 
 (provide 'config-emacs-defaults-language)
 ;;(set-default-coding-systems 'utf-8) 
