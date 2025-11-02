53eec1debf8d1d79082c85a09aede82c9ef7294a
wagnermarques <wagnerdocri@gmail.com>
2025-11-02 12:47:04 -0300

    plantuml and  menus

--- main/src/lispsite/fzl-ide-feat-menus.el
+++ main/src/lispsite/fzl-ide-feat-menus.el
@@ -6,6 +6,7 @@
 (provide 'fzl_menus)
 
 (require 'easymenu)
+(require 'fzl-util-browser)
 
 (easy-menu-define djcb-menu global-map "HOST"
   '("HOST"
@@ -21,6 +22,16 @@
 
 (easy-menu-define djcb-menu global-map "fzlbpms-menu"
   '("FZLBPMS"
+    ("Libre Office"
+     ("Bookmarks"
+      ("Templates"
+       ["www.libreofficetemplates.net" fzl-libreoffice-bookmarks--open-templates-site-in-browser("https://www.libreofficetemplates.net")]
+       ["Open Libre Office templates folder" fzl-open-libre-office-templates-folder]
+       ["Open Libre Office my templates folder" fzl-open-libre-office-my-templates-folder])
+      ("Extensions"
+       ["Open Libre Office extensions folder" fzl-open-libre-office-extensions-folder]))
+     ["Start Libre Office" fzl-start-libre-office]
+     ["Open Libre Office config file" fzl-open-libre-office-config-file])
     ("Utils"
      ["wkhtmltopdf" fzl-html2pdf])
     ("Containers" ;; submenu
@@ -214,5 +225,27 @@
 
        ;; http://emacs-fu.blogspot.com/2008/12/running-emacs-in-full-screen-mode.html
        ["Toggle full-screen" djcb-fullscreen-toggle])))
+
+
+(defun fzl-libreoffice-bookmarks--open-templates-site-in-browser (url)
+  "Opens the Libre Office templates site in browser."
+  (interactive)
+  (fzl-open-url-in-browser (url))
+
+(defun fzl-open-libre-office-templates-folder ()
+  "Opens the Libre Office templates folder."
+  (interactive)
+  ;;opens the default templates folder
+  (start-process
+   "ProcessName=LibreOfficeTemplatesFolder"
+   "BufferNameForTheProcess=LibreOfficeTemplatesFolder"
+   "xdg-open"
+   (concat (getenv "HOME") "/.config/libreoffice/4/user/templates/")))
+
+(defun fzl-open-libre-office-my-templates-folder ()
+  "Opens the Libre Office my templates folder."
+
+  )
+
 
 (provide 'fzl-ide-feat-menus)
